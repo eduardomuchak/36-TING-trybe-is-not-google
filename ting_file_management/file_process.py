373a67dfe84aaa123932da4577442acdbd06dc63
Eduardo Muchak <eduardomuchak@gmail.com>
2022-12-12 18:57:49 -0300

    feat: :sparkles: Implementa a função file_metadata dentro do módulo file_process

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,3 +1,4 @@
+import sys
 from ting_file_management.file_management import txt_importer
 from ting_file_management.queue import Queue
 
@@ -26,4 +27,7 @@ def remove(instance):
 
 
 def file_metadata(instance, position):
-    """Aqui irá sua implementação"""
+    try:
+        print(instance.search(position))
+    except IndexError:
+        print("Posição inválida", file=sys.stderr)
