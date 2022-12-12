937fe3da6598cf38f639e4ba89d1b5e3c14915e1
Eduardo Muchak <eduardomuchak@gmail.com>
2022-12-12 19:22:13 -0300

    feat: :sparkles: Implementa a função exists_word dentro do módulo word_search

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,5 +1,30 @@
-def exists_word(word, instance):
-    """Aqui irá sua implementação"""
+from ting_file_management.queue import Queue
+
+
+def exists_word(word, instance: Queue):
+    list = []
+
+    for index in range(len(instance)):
+        file = instance.search(index)
+        data = [
+            {"linha": index + 1}
+            for index, line in enumerate(file["linhas_do_arquivo"])
+            if word.lower() in line.lower()
+        ]
+
+        dict = {
+                    "palavra": word,
+                    "arquivo": file["nome_do_arquivo"],
+                    "ocorrencias": data
+                }
+
+        if dict["ocorrencias"] == []:
+            return []
+
+        if dict:
+            list.append(dict)
+
+    return list
 
 
 def search_by_word(word, instance):
